966fb62a602f4d22f1723620edf81fb7f18bc494
Igor Mayachenkov <igor.mayachenkov@gmail.com>
2022-11-25 13:07:03 +0300

    hourly/daily periodicity implemented

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 Capture images from yotube cameras according to the schedule
 ============================================================
+Configuration file format
+--------------------------
+| Parameter                 | Description                                                   |
+| :---------------          |:-----------------------------------------------------------   |
+| `schedule`                | periodic work settings.                                       |
+|   `.periodicity`          | "daily" or "hourly"                                           |
+|   `.times`                | times during the loop ("hh:mm" for daily, "mm:ss" for hourly)|
--- main.py
+++ main.py
@@ -6,9 +6,8 @@ import json
 
 print("============ image-capturer =============")
 
-last = datetime.datetime(2022,11,24,  19,00)
-#last = None
-print('last',last)
+# last = datetime.datetime(2022,11,24,  19,00)
+last = None
 
 def loadConfig():
     f = open('image-capturer-config.json')  # Opening JSON file
@@ -17,39 +16,63 @@ def loadConfig():
     #print(config)
     return config
 
-def checkSchedule(now, times):
-    if last==None : minutesLast = None
-    else:           minutesLast = 60*last.hour + last.minute
-    minutesNow  = 60*now.hour  + now.minute
-    print("minutesNow, minutessLast", now, minutesNow, minutesLast)
+class Hourly:
+    def timeOfInterval(self, dt)       : return 60*dt.minute + dt.second
+    def isNewInterval(self, last, now) : return last.hour != now.hour 
+
+class Daily:
+    def timeOfInterval(self, dt)       : return 60*dt.hour + dt.minute
+    def isNewInterval(self, last, now) : return last.day != now.day 
+
+def checkSchedule(scheduler, times,now):
+    if last==None : timeLast = None
+    else:           timeLast = scheduler.timeOfInterval(last)
+    timeNow  = scheduler.timeOfInterval(now)
+    print("timeNow, timeLast", timeNow, timeLast)
     for t in times:
         # parse the time in format "hh:mm"
         parts = t.split(':')
         h = int(parts[0])
         m = int(parts[1])
-        minutes = 60*h + m
-        print(parts,h,m,minutes)
-        if minutesLast!=None and minutesLast>=minutes : continue #this time has been played already
-        if minutesNow > minutes : return True
+        timeSched = 60*h + m #the formula is suitable for all periodicity values
+        print(parts,h,m,timeSched)
+        if timeLast!=None and timeLast>=timeSched : continue #this time has been played already
+        if timeNow > timeSched : return True
     return False
+
+def doAction():
+    print("doAction")
+    global last
+    last = datetime.datetime.now()
   
 def onTimer():
+    global last
+
     # Load config
     config = loadConfig()
-    times = config["schedule"]["times"]
+    periodicity = config["schedule"]["periodicity"]
+    times       = config["schedule"]["times"]
+    if periodicity=="hourly" : scheduler = Hourly()
+    if periodicity=="daily"  : scheduler = Daily()
 
     # Fix the current time
     now = datetime.datetime.now() #format like: 2022-11-24 18:14:24.447269
     # print(d,d.strftime("%d"))
+    print('----------------------------------------')
+    print('now ', now)
+    print('last',last)
 
-    doAction=checkSchedule(now,times)
-    print("doAction",doAction)
+    # clear last time if new interval started
+    if last!=None :
+        if scheduler.isNewInterval(last,now) :
+            last = None  
 
+    if checkSchedule(scheduler, times, now) : 
+        doAction()
 
 # THE LOOP
+while True:
+    onTimer()
+    time.sleep(60)
 
-# while True:
-#     onTimer()
-#     time.sleep(1)
-
-onTimer()
+#onTimer()
